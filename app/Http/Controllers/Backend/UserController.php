647d323b2606aae5f0b2a04df152911b92498820
cvsingh <cvsingh@rediffmail.com>
2022-01-07 10:25:09 +0530

    From Delhi

---
+++ app/Http/Controllers/Backend/UserController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function UserView() {
+
+       // $allData = User::all();
+       $data['allData'] = User::all();
+        return view('backend.user.view_user', $data); 
+    }
+
+    public function UserAdd() {
+        return view('backend.user.user_add');
+    }
+
+    public function UserStore(Request $request) {
+        $validatedData = $request->validate([
+            'email' => 'required|unique:users',
+            'name'  => 'required'
+        ]);
+
+        $data = new User();
+        $data->usertype = $request->usertype;
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->password = bcrypt($request->password);
+        $data->save();
+
+        $notification = array(
+            'message' => 'User Inserted Successfully.',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('user.view')->with($notification);
+
+    }
+}
